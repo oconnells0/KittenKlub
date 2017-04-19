5ec03b88e362b87b3c4d7fcc1898477d7a3a26e8
Michael Crinite <mikecrinite@gmail.com>
2017-04-18 23:37:48 -0400

    PHP

--- php/upload.php
+++ php/upload.php
@@ -1,3 +1,18 @@
-<?php>
+<?php
+  $title = $_POST["title"];
+  $description = $_POST["description"];
+  $file = $_FILES["submission"];
+
+  $filename = $file["tmp_name"];
+
+  //Make sure below works on every platform first
+  if (file_exists("./UPLOADED/archive/" . $_POST["username"])) {
+       echo "I see it already exists; you've uploaded before.</p>";
+  } else {
+       // bug in mkdir() requires you to chmod()
+       mkdir("./UPLOADED/archive/". $_POST["username"], 0777);
+       chmod("./UPLOADED/archive/". $_POST["username"], 0777);
+       echo "done.</p>";
+  }
 
 ?>
